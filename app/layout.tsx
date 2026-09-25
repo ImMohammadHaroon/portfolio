620be56a44b711952433ee3054337da955b7aee6
Muhammad Haroon <162042814+ImMohammadHaroon@users.noreply.github.com>
2026-09-25 15:41:45 +0500

    google analytics added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata, Viewport } from "next";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -24,6 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>{children}</body>
+      <GoogleAnalytics gaId="G-6QPKFSV2KN" />
     </html>
   );
 }
